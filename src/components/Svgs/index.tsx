90e28528b52f91b2c77e362a40632f8d43911260
Elian Avalos <elianemanuelstudy@gmail.com>
2023-03-06 10:07:15 -0300

    Add email and location svg

--- src/components/Svgs/index.tsx
+++ src/components/Svgs/index.tsx
@@ -3,12 +3,16 @@ import CloseSvg from "./CloseSvg";
 import CompoundSvg from "./CompoundSvg";
 import CvSvg from "./CvSvg";
 import DiarySvg from "./DiarySvg";
+import EmailSvg from "./EmailSvg";
 import GitHubSvg from "./GitHubSvg";
 import HomeSvg from "./HomeSvg";
+import LocationSvg from "./LocationSvg";
 import MenuSvg from "./MenuSvg";
 
 export {
   GitHubSvg,
+  LocationSvg,
+  EmailSvg,
   CvSvg,
   BookSvg,
   CloseSvg,
